4fc4263df5d3d48976094020c4aaa8dadfbde569
Enzo <enzo.rva@gmail.com>
2023-08-21 18:23:25 -0300

    Firebase initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:flash/screens/welcome_screen.dart';
 import 'package:flash/screens/login_screen.dart';
 import 'package:flash/screens/registration_screen.dart';
 import 'package:flash/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   const FlashChat({super.key});
